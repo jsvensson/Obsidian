c06869d39020b070d041c2f1d8c8adf405fb225c
Johan Svensson <johan@atomicplayboy.net>
2011-01-20 20:42:02 +0100

    Character class constant moved to Character model

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Obsidian::Application.initialize!
-
-# Classes for characters
-WOW_CLASSES = ['deathknight','druid','hunter','mage','rogue','paladin','priest','shaman','warlock','warrior']
