37a86b15094e366949825c1db2d4f7b5c2bc3114
Johan Svensson <johan@atomicplayboy.net>
2011-01-20 20:26:12 +0100

    Character class in model
    Validation for selecting a valid class for the character

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,19 @@
 class Character < ActiveRecord::Base
+  CLASSES = {
+    'Death Knight' => 'deathknight',
+    'Druid' => 'druid',
+    'Hunter' => 'hunter',
+    'Mage' => 'mage',
+    'Rogue' => 'rogue',
+    'Paladin' => 'paladin',
+    'Priest' => 'priest',
+    'Shaman' => 'shaman',
+    'Warlock' => 'warlock',
+    'Warrior' => 'warrior' 
+  }
+
   belongs_to :player
   belongs_to :guild
-  validates :name,  :presence => true
-  validates :class, :presence => true
+  validates :name,      :presence => true
+  validates_inclusion_of :wowclass, :in => CLASSES.values
 end
