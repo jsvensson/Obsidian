cbcf401b5fdf1e6b7e520b59fd169ef91b20edd2
Johan Svensson <johan@atomicplayboy.net>
2011-01-23 21:51:14 +0100

    Gasp! Commented code!

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,5 +1,7 @@
 module CharactersHelper
   def readable_class(wowclass)
+    # CLASSES is a hash with readable name as key and string as value for easy select()
+    # purposes. Reverse it to make it less of a hassle to refer to readable name in views.
     Character::CLASSES.invert[@character.wowclass]
   end
 end
